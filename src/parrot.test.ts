ab8e2ed7538ca40fb48aa575894bb0274fb07d11
André Mathlener <andre.mathlener@ns.nl>
2025-04-16 15:37:43 +0200

    feat: add code and unit tests

---
+++ src/parrot.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, it } from 'vitest';
+import { Parrot, ParrotTypes } from './parrot.ts';
+
+describe('Parrot Refactoring Kata', () => {
+    it('gets speed of European Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        expect(parrot.getSpeed()).toBe(12);
+    });
+
+    it('gets speed of African Parrot with one coconut', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        expect(parrot.getSpeed()).toBe(3);
+    });
+
+    it('gets speed of African Parrot with two coconuts', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
+        expect(parrot.getSpeed()).toBe(0);
+    });
+
+    it('gets speed of African Parrot with no coconuts', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
+        expect(parrot.getSpeed()).toBe(12);
+    });
+
+    it('gets speed of Norwegian Blue Parrot nailed', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+        expect(parrot.getSpeed()).toBe(0);
+    });
+
+    it('gets speed of Norwegian Blue Parrot not nailed', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+        expect(parrot.getSpeed()).toBe(18);
+    });
+
+    it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        expect(parrot.getSpeed()).toBe(24);
+    });
+
+    it('gets cry of European Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        expect(parrot.getCry()).toBe('Sqoork!');
+    });
+
+    it('gets cry of African Parrot', () => {
+        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        expect(parrot.getCry()).toBe('Sqaark!');
+    });
+    it('gets cry of Norwegian Blue with high voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        expect(parrot.getCry()).toBe('Bzzzzzz');
+    });
+
+    it('gets cry of Norwegian Blue without voltage', () => {
+        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
+        expect(parrot.getCry()).toBe('...');
+    });
+});
